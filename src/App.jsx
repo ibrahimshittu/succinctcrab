45527eb040b8ff7dc28f20cb6a04bb40cf0297af
Tolex081 <78969074+Tolex081@users.noreply.github.com>
2025-06-19 14:17:06 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,6 @@ const App = () => {
   return (
     <div className="app-container">
     <h1 style={{ color: 'red' }}>Hello from App.jsx</h1>
-    {/* ... rest of your game logic ... */}
   </div>
     
     <div className="app-container">
